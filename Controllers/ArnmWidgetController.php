fa9032cd924d6dc103a4bd2d48cffb79e9549702
ubuntu <alex@iibspro.com>
2014-06-11 02:33:28 -0700

    Finished plugging in widget configuration flow.

--- Controllers/ArnmWidgetController.php
+++ Controllers/ArnmWidgetController.php
@@ -1,8 +1,10 @@
 <?php
 namespace Arnm\WidgetBundle\Controllers;
 
+use Symfony\Component\HttpKernel\Exception\BadRequestHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Arnm\WidgetBundle\Entity\Param;
 use Arnm\WidgetBundle\Entity\Widget;
-
 use Symfony\Component\HttpFoundation\Response;
 use Arnm\CoreBundle\Controllers\ArnmAjaxController;
 /**
@@ -24,61 +26,53 @@ abstract class ArnmWidgetController extends ArnmAjaxController
     abstract public function renderAction(Widget $widget);
 
     /**
-     * Responds to an ajax request for edit form for a widget
-     *
-     * @param int $id
+     * Renders widget config form template
      *
      * @return Response
      */
-    abstract public function editAction($id);
+    abstract public function editAction();
+
+    /**
+     * Renders widget's data in json format
+     *
+     * @param int $id
+     */
+    public function dataAction($id)
+    {
+        $widgetsMgr = $this->getWidgetManager();
+        $widget = $widgetsMgr->findWidgetById($id);
+        if (!($widget instanceof Widget)) {
+            throw $this->createNotFoundException("Widget with id: '" . $id . "' not found!");
+        }
+
+        $data = array();
+        foreach ($this->getConfigFields() as $field) {
+            $param = $widget->getParamByName($field);
+            $data[$field] = ( ($param instanceof Param) ? $param->getValue() : '' );
+        }
+
+        return $this->createResponse($data);
+    }
+
+    /**
+     * Gets config fields that must be returned in the model with data
+     *
+     * @return array
+     */
+    public function getConfigFields()
+    {
+        return array();
+    }
 
     /**
      * Handles widget edit form submittion
      *
      * @param int $id
+     * @param Request $request
      *
      * @return Response
      */
-    abstract public function updateAction($id);
-
-    /**
-     * Handles widget deletion request
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
-    public function deleteAction($id)
-    {
-        $this->validateRequest();
-
-        $reply = array();
-        $em = $this->getEntityManager();
-        $em->getConnection()->beginTransaction();
-        try {
-            $widget = $this->getWidgetManager()->findWidgetById($id);
-            if (! ($widget instanceof Widget)) {
-                throw new \InvalidArgumentException("Widget was not found by ID.");
-            }
-
-            foreach ($widget->getParams() as $param) {
-                $em->remove($param);
-            }
-            $em->remove($widget);
-
-            $reply['status'] = 'OK';
-            $reply['id'] = $id;
-            //flush
-            $em->flush();
-            $em->getConnection()->commit();
-        } catch (\Exception $e) {
-            $em->getConnection()->rollback();
-            $reply['sratus'] = 'FAILED';
-            $reply['reson'] = $e->getMessage();
-        }
-
-        return $this->createResponse($reply);
-    }
+    abstract public function updateAction($id, Request $request);
 
     /**
      * Gets an instance of widget manager
@@ -88,5 +82,27 @@ abstract class ArnmWidgetController extends ArnmAjaxController
     protected function getWidgetManager()
     {
         return $this->get('arnm_widget.manager');
+    }
+
+    /**
+     * Extracts an array based on content passed in request as json
+     *
+     * @throws BadRequestHttpException
+     *
+     * @return array
+     */
+    protected function extractArrayFromRequest(Request $request)
+    {
+        $content = $request->getContent();
+        if (empty($content)) {
+            throw new BadRequestHttpException("Empty payload!");
+        }
+
+        $data = json_decode($content, true);
+        if (!is_array($data)) {
+            throw new BadRequestHttpException("Payload is not parsable!");
+        }
+
+        return $data;
     }
 }
